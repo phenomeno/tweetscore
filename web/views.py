921be5d8995926cde3d8f41534a075d76564a4f5
Grace Lee <writegracelee@gmail.com>
2016-03-05 14:43:05 -0800

    Added in some fake data to fill out the view.

--- web/views.py
+++ web/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'web/index.html', {})
+    data = {
+        "photo_link": "http://thelistenersclub.timothyjuddviolin.com/wp-content/uploads/sites/2/2014/08/small_waves_1920x1200.jpg",
+        "name": "Bob",
+        "handle": "bob",
+        "description": "welcome to my twitter page",
+        "location": "santa monica",
+        "external_link": "www.mypace.com",
+        "join_date": "Feb 2, 1992",
+        "tweetscore": 740,
+        "follower_count": 33,
+        "tweet_content": 0.8,
+        "followers_score": 0.3,
+        "tweets": [
+            {
+            "retweet_count": 3,
+            "media": "http://thelistenersclub.timothyjuddviolin.com/wp-content/uploads/sites/2/2014/08/small_waves_1920x1200.jpg",
+            "text": "My fake tweet"
+            }
+        ]
+    }
+    return render(request, 'web/index.html', {"data": data})
