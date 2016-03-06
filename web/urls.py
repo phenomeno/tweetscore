82fc41df3efbf0ff155509558aaaf90353f6235f
Grace Lee <writegracelee@gmail.com>
2016-03-06 00:24:33 -0800

    Added in another endpoint to retrieve twitter data from. WIP but going to bed so checking in.

--- web/urls.py
+++ web/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     url(r'^', views.index, name='index'),
+    url(r'^twitter_data\\?(?P<username>[a-zA-Z0-9._]+)$', views.twitter_data, name="twitter_data")
 ]
--- web/views.py
+++ web/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+import json, os, pprint
+from application_only_auth import Client
 
 def index(request):
+    return render(request, 'web/index.html', {})
+
+def twitter_data(request):
+    CONSUMER_KEY = os.environ["CONSUMER_KEY"]
+    CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
+    client = Client(CONSUMER_KEY, CONSUMER_SECRET)
+    user = client.request('https://api.twitter.com/1.1/users/show.json?screen_name=barackobama')
+
+    # Delete later
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(user)
+
+    # Preprocess image to get larger resolution
+    normal_index = user["profile_image_url"].find("normal.png")
+
     data = {
-        "photo_link": "http://thelistenersclub.timothyjuddviolin.com/wp-content/uploads/sites/2/2014/08/small_waves_1920x1200.jpg",
+        "photo_link": user["profile_image_url_https"],
         "name": "Bob",
         "handle": "bob",
         "description": "welcome to my twitter page",
@@ -21,4 +40,4 @@ def index(request):
             }
         ]
     }
-    return render(request, 'web/index.html', {"data": data})
+    return JsonResponse({'hello': 'world'})
