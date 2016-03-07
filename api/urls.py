895b3683af306cbad9f13122254f7bc589d3dc9b
Grace Lee <writegracelee@gmail.com>
2016-03-07 05:16:00 -0800

    Changed endpoint name to reflect that it will send user and tweet data.

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^v1/users/(?P<screen_name>[a-zA-Z0-9._]+)$', views.get_user, name="get_user"),
-    url(r'^v1/users/(?P<screen_name>[a-zA-Z0-9._]+)/tweets$', views.get_tweets, name="get_tweets"),
-    url(r'^test_cache', views.test_cache),
+    url(r'^v1/users/(?P<screen_name>[a-zA-Z0-9._]+)/twitter_data$', views.twitter_data, name="twitter_data"),
 ]
