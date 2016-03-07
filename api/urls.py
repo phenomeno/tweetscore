6e3e0d06cfbad6ef433fb47048027ecbd3df22a5
Grace Lee <writegracelee@gmail.com>
2016-03-06 23:23:40 -0800

    Changed routing. Not sure what the final routes should look like yet..

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^api/v1/users/(?P<screen_name>[a-zA-Z0-9._]+)$', views.get_user, name="get_user"),
-    url(r'^api/v1/users/(?P<screen_name>[a-zA-Z0-9._]+)/tweets$', views.get_tweets, name="get_tweets"),
-    url(r'^api/v1/users/(?P<screen_name>[a-zA-Z0-9._]+)/friends$', views.get_friends, name="get_friends"),
-    url(r'^api/v1/users\\?(?P<user_id>[0-9]+)$', views.get_users, name="get_users"),
+    url(r'^v1/users/(?P<screen_name>[a-zA-Z0-9._]+)$', views.get_user, name="get_user"),
+    url(r'^v1/users/(?P<screen_name>[a-zA-Z0-9._]+)/tweets$', views.get_tweets, name="get_tweets"),
 ]
