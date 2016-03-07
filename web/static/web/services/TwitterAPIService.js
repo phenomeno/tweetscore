6b88503f304c351ea5849f7728caad4de154c382
Grace Lee <writegracelee@gmail.com>
2016-03-06 23:46:31 -0800

    Basic service that sends requests to endpoint in Django to query data from twitter.

---
+++ web/static/web/services/TwitterAPIService.js
@@ -0,0 +1,13 @@
+angular.module('tweetscore').service('TwitterAPIService', function($http) {
+  this.getUserData = function(screen_name, callback) {
+    $http.get("/api/v1/users/"+screen_name).
+    success(function(data){
+      console.log(data);
+      callback(data);
+    }).
+    error(function(error) {
+      console.error(error);
+      callback(null);
+    });
+  };
+});
