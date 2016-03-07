c50d9b1a87677cf97a3baf139e3aba6ac6e8b64f
Grace Lee <writegracelee@gmail.com>
2016-03-07 01:09:01 -0800

    Added temporary endpoint to test redis cache.

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     url(r'^v1/users/(?P<screen_name>[a-zA-Z0-9._]+)$', views.get_user, name="get_user"),
     url(r'^v1/users/(?P<screen_name>[a-zA-Z0-9._]+)/tweets$', views.get_tweets, name="get_tweets"),
+    url(r'^test_cache', views.test_cache),
 ]
