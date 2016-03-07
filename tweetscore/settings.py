f3b4db6ccd456570d2e23c2d7081c865d101af3e
Grace Lee <writegracelee@gmail.com>
2016-03-07 01:08:09 -0800

    Added in cache settings for redis.

--- tweetscore/settings.py
+++ tweetscore/settings.py
@@ -129,7 +129,7 @@ CACHES = {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": "redis://127.0.0.1:6379/1",
         "OPTIONS": {
-            "CLIENT_CLASS": django_redis.client.DefaultClient",
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
     }
 }
