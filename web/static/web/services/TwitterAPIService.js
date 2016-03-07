b7d56cc921af13aeab4c564a33f4325d95479ab4
Grace Lee <writegracelee@gmail.com>
2016-03-07 05:16:49 -0800

    Change API endpoint to reflect name change.

--- web/static/web/services/TwitterAPIService.js
+++ web/static/web/services/TwitterAPIService.js
@@ -1,6 +1,6 @@
 angular.module('tweetscore').service('TwitterAPIService', function($http) {
   this.getUserData = function(screen_name, callback) {
-    $http.get("/api/v1/users/"+screen_name).
+    $http.get("/api/v1/users/"+screen_name+"/twitter_data").
     success(function(data){
       console.log(data);
       callback(data);
