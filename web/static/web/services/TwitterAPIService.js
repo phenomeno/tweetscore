4bf0d88fa4f8744baa3c2b09a462132ba92cfab3
Grace Lee <writegracelee@gmail.com>
2016-03-08 02:00:28 -0800

    Add in filters as parameters sent as query strings.

--- web/static/web/services/TwitterAPIService.js
+++ web/static/web/services/TwitterAPIService.js
@@ -1,8 +1,17 @@
 angular.module('tweetscore').service('TwitterAPIService', function($http) {
-  this.getUserData = function(screen_name, callback) {
-    $http.get("/api/v1/users/"+screen_name+"/twitter_data").
+  this.getUserData = function(screen_name, retweet_count, picture_toggle, date_start, date_end, callback) {
+    $http.get(
+      "/api/v1/users/"+screen_name+"/twitter_data",
+      config= {
+        params: {
+          retweet_count: retweet_count,
+          picture_toggle: picture_toggle,
+          date_start: date_start,
+          date_end: date_end
+        }
+      }
+    ).
     success(function(data){
-      console.log(data);
       callback(data);
     }).
     error(function(error) {
