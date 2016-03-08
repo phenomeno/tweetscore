720186aaaa3917429e86b000f0a86ac36811f3cd
Grace Lee <writegracelee@gmail.com>
2016-03-08 01:58:04 -0800

    Backend API now handles filtering logic. Still set to use cached data so should switch it to actually query Twitter's API.

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse, HttpResponse
 from django.core.cache import cache
 
-import json, os, pprint, re, csv
+import json, os, pprint, re, csv, calendar, time, datetime, email.utils
 from application_only_auth import Client
 
 
@@ -14,83 +14,132 @@ negative_words = []
 positive_words = []
 
 def twitter_data(request, screen_name):
-    # # Check if user exists in cache
+    # Query strings for tweet filter
+    picture_toggle = request.GET.get('picture_toggle')
+    retweet_count = request.GET.get('retweet_count')
+    date_start = request.GET.get('date_start', 0)
+    date_end = request.GET.get('date_end', calendar.timegm(datetime.datetime.utcnow().utctimetuple()))
+
+    # Check if user exists in cache
     user = cache.get(screen_name)
-    if user is None:
-        # FIXME do retweets hold retweeted user object?
-        # tweets = client.request('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name='+screen_name+'&count=2')
+    tweets = cache.get(screen_name+':tweets')
+
+    if user is None or tweets is None:
+        # tweets = client.request('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name='+screen_name+'&count=200')
         tweets = cache.get("temp_data")
-        print "length of tweets list", len(tweets)
-        positive_total, negative_total, words_total = 0, 0, 0
+        twitter_score = get_twitter_score(screen_name, tweets)
+        user = tweets[0].get("user")
+        user["twitter_score"] = twitter_score
+        cache.set(screen_name, user, timeout=86400)
+        cache.set(screen_name+':tweets', tweets, timeout=86400)
+
+    # Filter tweets if need be
+    if picture_toggle == "on":
         for tweet in tweets:
-            score = get_tweet_score(tweet)
-            positive_total += score[0]
-            negative_total += score[1]
-            words_total += score[2]
-        print positive_total, negative_total, words_total
-        content_score = (positive_total - negative_total) / words_total
-        print content_score
-        # friends_count = tweets[0].get("user").get("friends_count")
-        # friends_ids = get_friends(screen_name)
-        # friends_user_objects = get_users(friends_ids)
-        # friends_total_score = 0
-        # for friend in friends_user_objects:
-        #     friend_score = get_friend_score(friend)
-        #     friend["friend_score"] = friend_score
-        #     cache.set(friend.get("screen_name"), friend, timeout=86400)
-        #     friends_total_score += friends_score
-        # friends_average = friends_score / len(friends_user_objects)
-        # twitter_score = ((friends_average + friends_count) / 2 ) * content_score
-        # user = tweets[0].get("user")
-        # user["twitter_score"] = twitter_score
-        # cache.set(screen_name, user, timeout=86400)
-    # return JsonResponse(user)
+            if not tweet.get("entities").get("media"):
+                tweets.remove(tweet)
+
+    try:
+        retweet_count = int(retweet_count)
+        for tweet in tweets:
+            if tweet.get("retweet_count") != retweet_count:
+                tweets.remove(tweet)
+    except:
+        pass
+
+    for tweet in tweets:
+        created_at = calendar.timegm(email.utils.parsedate(tweet.get("created_at")))
+        if created_at < date_start or created_at > date_end:
+            tweets.remove(tweet)
+
+    return JsonResponse({'user': user, 'tweets': tweets})
 
 
-def get_friends(screen_name):
-    # This gets you 5,000 user ids. Use friends.get("next_cursor") to get next page.
-    friends = client.request('https://api.twitter.com/1.1/friends/ids.json?screen_name='+screen_name)
-    return friends.get("ids")
+def get_followers(screen_name):
+    # This gets you 5,000 user ids. Use followers.get("next_cursor") to get next page.
+    followers = client.request('https://api.twitter.com/1.1/followers/ids.json?screen_name='+screen_name+'&count=100')
+    return followers.get("ids")
 
 
 def get_users(user_ids):
+    # 100 users per request, 60 per 15 min.. so if we do all 5000, thats 50 requests. Takes up all the requests...
+    # Maybe just limit to follower scores to only 100 of them.
     user_ids_as_string = ','.join(map(str, user_ids))
     users = client.request('https://api.twitter.com/1.1/users/lookup.json?user_id='+user_ids_as_string)
     return users
 
 
+def get_twitter_score(screen_name, tweets):
+    content_score = get_content_score(tweets)
+    followers_count = tweets[0].get("user").get("followers_count")
+    followers_average = get_average_follower_score(screen_name)
+    twitter_score = ((followers_average + followers_count) / 2 ) * content_score
+    return twitter_score
+
+
 def get_tweet_score(tweet):
     """ Returns list consisting of positive word count, negative word count, and total word count of single tweet. """
+    if tweet:
+        negative_words = cache.get("negative_words")
+        positive_words = cache.get("positive_words")
+        if negative_words is None or positive_words is None:
+            load_word_lists()
+            negative_words = cache.get("negative_words")
+            positive_words = cache.get("positive_words")
+        text = tweet.get("text")
+        if text:
+            text = re.sub('[^a-zA-Z0-9_ ]', '', text)
+            words = text.split(' ')
+            pos_count, neg_count = 0, 0
+            for word in words:
+                for pos in positive_words:
+                    if pos[0] in word:
+                        pos_count += 1
+                for neg in negative_words:
+                    if neg[0] in word:
+                        neg_count += 1
+            return [pos_count, neg_count, len(words)]
+        else:
+            return [0, 0, 0]
 
-    negative_words = cache.get("negative_words")
-    positive_words = cache.get("positive_words")
-    if negative_words is None or positive_words is None:
-        load_word_lists()
-    text = tweet.get("text")
-    if text:
-        text = re.sub('\W', '', text)
-        words = text.split(' ')
-        pos_count, neg_count = 0, 0
-        for word in words:
-            for pos in positive_words:
-                print type(pos)
-                print pos
-                if pos in word:
-                    pos_count += 1
-            for neg in negative_words:
-                if neg in word:
-                    neg_count += 1
-        return [pos_count, neg_count, len(text)]
-    else:
-        return [0, 0, 0]
+
+def get_content_score(tweets):
+    positive_total, negative_total, words_total = 0, 0, 0
+    for tweet in tweets:
+        score = get_tweet_score(tweet)
+        positive_total += score[0]
+        negative_total += score[1]
+        words_total += score[2]
+    content_score = ((positive_total - negative_total) / float(words_total) + 1 )/ 2
+    return content_score
 
 
-def get_friend_score(user_object):
-    friends_count = user_object.get("friends_count")
+def get_follower_score(user_object):
+    followers_count = user_object.get("followers_count")
     latest_tweet = user_object.get("status")
-    latest_tweet_score = get_tweet_score(latest_tweet)
-    content_score = (latest_tweet_score[0] - latest_tweet_score[1]) / latest_tweet_score[2]
-    return content_score * friends_count
+    content_score = 0
+    if latest_tweet:
+        latest_tweet_score = get_tweet_score(latest_tweet)
+        content_score = (latest_tweet_score[0] - latest_tweet_score[1]) / float(latest_tweet_score[2])
+    return content_score * followers_count
+
+
+def get_average_follower_score(screen_name):
+    followers_ids = get_followers(screen_name)
+    followers_user_objects = get_users(followers_ids)
+    followers_total_score = 0
+    for follower in followers_user_objects:
+        cached_follower = cache.get(follower.get("screen_name"))
+        if cached_follower:
+            followers_total_score += int(cached_follower.get("follower_score"))
+        else:
+            follower_score = get_follower_score(follower)
+            follower["follower_score"] = follower_score
+            cache.set(follower.get("screen_name"), follower, timeout=86400)
+            followers_total_score += follower_score
+    followers_average = followers_total_score / len(followers_user_objects)
+    return followers_average
+
 
 def load_word_lists():
     wd =  os.path.dirname(__file__)
@@ -98,14 +147,12 @@ def load_word_lists():
     pw = os.path.join(wd, 'word_lists/positive-words.txt')
     with open(nw) as file:
         for row in csv.reader(file):
-            print row
-            if len(row) != 0:
-                if row[0] != ";":
-                    negative_words.append(row)
+            if len(row) != 0 and row[0][0] != ";":
+                negative_words.append(row)
     cache.set("negative_words", negative_words)
     with open(pw) as file:
         reader = csv.reader(file)
         for row in csv.reader(file):
-            if row[0] != ";":
+            if len(row) != 0 and row[0] != ";":
                 positive_words.append(row)
     cache.set("positive_words", positive_words)
