4f9bd0d7bd91a9baac5ebc03caa31a2a77f7be60
Grace Lee <writegracelee@gmail.com>
2016-03-08 16:45:13 -0800

    Fiddling with namespace so it'll work with tests.

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
-
-
 from . import views
+
 
 urlpatterns = [
     url(r'^v1/users/(?P<screen_name>[a-zA-Z0-9._]+)/twitter_data$', views.twitter_data, name="twitter_data"),
