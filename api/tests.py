42d8889ec8dbe13e402245eec46326f876bb009c
Grace Lee <writegracelee@gmail.com>
2016-03-08 16:44:53 -0800

    Needs work..

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+
+class APIViewTests(TestCase):
+    def test_api_get(self):
+        """If api is called, must return a user and tweets."""
+        response = self.client.get(reverse('twitter_data'), kwargs={'picture_toggle': 'all', 'retweet_count': 'all'})
+        self.assertEqual(response.status_code, 200)
+        print response
+        print response.context['user']
+        self.assertTrue(response.context['user'])
