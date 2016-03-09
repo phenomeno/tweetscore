266cb10ef1cd250a4025b0c9b4c3b376eaebd42d
Grace Lee <writegracelee@gmail.com>
2016-03-08 19:05:03 -0800

    Remove namespace names.

--- tweetscore/urls.py
+++ tweetscore/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('web.urls', namespace='web')),
-    url(r'^api/', include('api.urls', namespace='api')),
+    url(r'^$', include('web.urls')),
+    url(r'^api/', include('api.urls')),
     url(r'^admin/', admin.site.urls),
 ]
